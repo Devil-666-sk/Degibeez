f9721dca8a5d9f9a17a5fc82fa74433d7abfe2fe
Shubham Kumar <sk6930037@gmail.com>
2024-12-07 13:54:26 +0530

    changes

--- digibeez/src/App.jsx
+++ digibeez/src/App.jsx
@@ -4,12 +4,9 @@ import AboutDigiBeez from './Components/About/AboutDigiBeez';
 import Grow from './Components/Grow/Grow';
 import Career from './components/career/career';
 import VisionMission from './Components/VisionandMission/VisionMission';
-<<<<<<< HEAD
 import Footer from './Components/Footer/Footer';
 
-=======
 import DigitalMarketing from './Components/DigitalMarketing/DigitalMarketing';
->>>>>>> 56ce5330400bc63351f8450526a7fa707333f2c9
 function App() {
   return (
     <>
@@ -21,7 +18,7 @@ function App() {
       <DigitalMarketing />
       <Career />
       <Footer/>
->>>>>>> 56ce5330400bc63351f8450526a7fa707333f2c9
+
     </>
   );
 }
